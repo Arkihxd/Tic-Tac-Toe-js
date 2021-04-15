e395ad88313f740632897d3b3d71cd8b80f2cee7
Mateus Oliveira <mateusoliveiradesouza@hotmail.com>
2021-04-15 20:52:56 -0300

    Lógica do jogo implementada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,77 @@
 const ticTacToe = {
     board: ['', '', '', '', '', '', '', '', ''],
-    simbols: ['X', 'O'],
+    simbols:{
+        options: ['X', 'O'],
+        turnIndex: 0,
+        change: function(){
+            this.turnIndex = (this.turnIndex === 0 ? 1: 0);
+        }
+    },
     containerElement: null,
+    gameOver:false,
+    winSequences: [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ],
 
     init: function(container){
         this.containerElement = container;
     },
 
+    start: function(){
+        this.board.fill('');
+        this.draw();
+        this.gameOver = false;
+    },
+
+    play: function(position){
+        console.log(position)
+        if(this.gameOver) return false;
+        if(this.board[position] === ''){
+            this.board[position] = this.simbols.options[this.simbols.turnIndex];
+            this.draw();
+            let winSequencesIndex = this.checkWinSequences(this.simbols.options[this.simbols.turnIndex]);
+            if(winSequencesIndex >= 0){
+                let winner = this.simbols.options[this.simbols.turnIndex];
+                this.finalGame(winner);
+            }
+            else{
+                this.simbols.change();
+            }
+            return true;
+        }
+        else{
+            alert('Jogada Inválida');
+            return false;
+        }
+    },
+
+    finalGame: function(winner){
+        this.gameOver = true;
+        alert(`${winner} Ganhou`);
+    },
+
+    checkWinSequences: function(symbol){
+        for (i in this.winSequences){
+            if(this.board[this.winSequences[i][0]] == symbol && 
+               this.board[this.winSequences[i][1]] == symbol &&
+               this.board[this.winSequences[i][2]] == symbol){
+                    return i;
+               } 
+        };
+        return -1;
+    },
+
     draw: function(){
         let content = '';
-        for (let i in this.board) {
-            content += `<div>${i}</div>`
+        for (i in this.board) {
+            content += `<div onclick="ticTacToe.play(${i})">${this.board[i]}</div>`
         }
         this.containerElement.innerHTML = content;
     }
@@ -18,4 +79,4 @@ const ticTacToe = {
 
 const game = document.querySelector('.game')
 ticTacToe.init(game);
-ticTacToe.draw();
+ticTacToe.start();
